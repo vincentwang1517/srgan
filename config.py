35283afff34127b8676a3df568b5496d428c67be
vincentwang1517 <vin84526@gmail.com>
2019-02-11 16:33:24 +0800

    modify config.py

--- config.py
+++ config.py
@@ -5,28 +5,30 @@ config = edict()
 config.TRAIN = edict()
 
 ## Adam
-config.TRAIN.batch_size = 16
-config.TRAIN.lr_init = 1e-4
+config.TRAIN.batch_size = 128
+config.TRAIN.lr_init = 1e-3
 config.TRAIN.beta1 = 0.9
 
 ## initialize G
-config.TRAIN.n_epoch_init = 100
+config.TRAIN.n_epoch_init = 30
     # config.TRAIN.lr_decay_init = 0.1
     # config.TRAIN.decay_every_init = int(config.TRAIN.n_epoch_init / 2)
 
 ## adversarial learning (SRGAN)
-config.TRAIN.n_epoch = 2000
+config.TRAIN.n_epoch = 100
 config.TRAIN.lr_decay = 0.1
 config.TRAIN.decay_every = int(config.TRAIN.n_epoch / 2)
 
 ## train set location
-config.TRAIN.hr_img_path = 'data2017/DIV2K_train_HR/'
-config.TRAIN.lr_img_path = 'data2017/DIV2K_train_LR_bicubic/X4/'
+config.TRAIN.hr_img_path = '../data2017/fer2013_train.csv'
+# config.TRAIN.lr_img_path = 'data2017/DIV2K_train_LR_bicubic/X4/'
 
 config.VALID = edict()
 ## test set location
-config.VALID.hr_img_path = 'data2017/DIV2K_valid_HR/'
-config.VALID.lr_img_path = 'data2017/DIV2K_valid_LR_bicubic/X4/'
+config.VALID.hr_img_path = '../data2017/fer2013_valid.csv'
+# config.VALID.lr_img_path = 'data2017/fer2013_test.csv'
+
+config.MODEL_path = 'checkpoint/g_srgan.npz'
 
 def log_config(filename, cfg):
     with open(filename, 'w') as f:
