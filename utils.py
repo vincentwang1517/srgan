9e21f75fd054d471790c3dcc3cb19d4a95fa9fef
vincentwang1517 <vin84526@gmail.com>
2019-02-11 16:01:36 +0800

    modify utils.py

--- utils.py
+++ utils.py
@@ -7,6 +7,7 @@ from tensorlayer.prepro import *
 
 import scipy
 import numpy as np
+import pandas as pd
 
 def get_imgs_fn(file_name, path):
     """ Input an image path and name, return an image array """
@@ -14,16 +15,38 @@ def get_imgs_fn(file_name, path):
     return scipy.misc.imread(path + file_name, mode='RGB')
 
 def crop_sub_imgs_fn(x, is_random=True):
-    x = crop(x, wrg=384, hrg=384, is_random=is_random)
     x = x / (255. / 2.)
     x = x - 1.
-    # x = (x - 0.5)*2
     return x
 
-def downsample_fn(x):
+def downsample_fn(x, down_rate=3):
     # We obtained the LR images by downsampling the HR images using bicubic kernel with downsampling factor r = 4.
-    x = imresize(x, size=[96, 96], interp='bicubic', mode=None)
+    down_width = int(x.shape[1] / down_rate)
+    down_height = int(x.shape[0] / down_rate) 
+    x = imresize(x, size=[down_width, down_height], interp='bicubic', mode=None)
     x = x / (255. / 2.)
     x = x - 1.
-    # x = (x - 0.5)*2
     return x
+
+def upsample_fn(x, up_rate=3):
+    # We obtained the LR images by downsampling the HR images using bicubic kernel with downsampling factor r = 4.
+    up_width = int(x.shape[1] * up_rate)
+    up_height = int(x.shape[0] * up_rate) 
+    x = imresize(x, size=[up_width, up_height], interp='bicubic', mode=None)
+    x = x / (255. / 2.)
+    x = x - 1.
+    return x
+
+def read_csv_data(file_path, width, height, channel=1):
+    print('--- Read Image ---')
+    data = pd.read_csv(open(file_path, 'rb')).as_matrix()
+    img = np.zeros((len(data), width*height))
+    for i in range(len(data)):
+        try:
+            img[i] = data[i,1].split();
+        except:
+            print(i)
+    img = img.astype('float32')
+    img = np.resize(img, (len(data), width, height, channel))
+    print(img.shape)
+    return img
